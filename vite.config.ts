792486b4cc3e392fee6b4d108608d8da7e0d9c1a
gharghiz <gharghiz34@gmail.com>
2025-09-29 13:37:47 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/zenchain-swap/', // ← ⚠️ بدّل الاسم حسب الريبو ديالك
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
